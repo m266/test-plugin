9d62d3364d87de1d9a56c4b44f1a100e64e40e2d
m266 <herbrand@mailbox.org>
2024-05-23 23:01:03 +0200

    1.1

---
+++ test-plugin - Kopie.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name:        Test-Plugin
+Plugin URI:         https://github.com/m266/test-plugin
+Description:        Test-Plugin
+Author:             Hans M. Herbrand
+Author URI:         https://www.herbrand.org
+Version:            1.0
+Date:               2024-05-23
+Text Domain:        test-plugin
+License:            GNU General Public License v2 or later
+License URI:        http://www.gnu.org/licenses/gpl-2.0.html
+GitHub Plugin URI:  https://github.com/m266/test-plugin
+ */
+
+// Plugin-Code
--- test-plugin.php
+++ test-plugin.php
@@ -5,7 +5,7 @@ Plugin URI:         https://github.com/m266/test-plugin
 Description:        Test-Plugin
 Author:             Hans M. Herbrand
 Author URI:         https://www.herbrand.org
-Version:            1.0
+Version:            1.1
 Date:               2024-05-23
 Text Domain:        test-plugin
 License:            GNU General Public License v2 or later
@@ -14,3 +14,25 @@ GitHub Plugin URI:  https://github.com/m266/test-plugin
  */
 
 // Plugin-Code
+// Externer Zugriff verhindern
+defined('ABSPATH') || exit();
+// Stand: 23.05.2024
+// Check, ob Plugin aktiv/inaktiv ist
+// Plugin "Test-Plugin" aktiv?
+if (!function_exists('is_plugin_active')) {
+require_once ABSPATH . '/wp-admin/includes/plugin.php';
+}
+if (is_plugin_active('test-plugin/test-plugin.php')) {
+function test_plugin_notice() { ?>
+<div class="notice notice-error">
+<p><?php _e('Die Entwicklung des Plugins "Test-Plugin" wurde eingestellt. Bitte nach dieser Anleitung vorgehen: https://herbrand.org/wordpress/eigene-plugins/wp-h-data-protection/!');?></p>
+</div>
+<?php
+}
+add_action( 'load-index.php',
+function(){
+add_action( 'admin_notices', 'test_plugin_notice' );
+}
+);
+}
+?>
