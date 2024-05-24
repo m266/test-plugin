1213c528d348b028d65f9282b2dff72975e2b614
m266 <herbrand@mailbox.org>
2024-05-24 16:16:03 +0200

    1.3

--- test-plugin.php
+++ test-plugin.php
@@ -6,7 +6,7 @@ Description:        Test-Plugin
 Author:             Hans M. Herbrand
 Author URI:         https://www.herbrand.org
 Version:            1.3
-Date:               2024-05-23
+Date:               2024-05-24
 Text Domain:        test-plugin
 License:            GNU General Public License v2 or later
 License URI:        http://www.gnu.org/licenses/gpl-2.0.html
@@ -16,14 +16,13 @@ GitHub Plugin URI:  https://github.com/m266/test-plugin
 // Plugin-Code
 // Externer Zugriff verhindern
 defined('ABSPATH') || exit();
-// Erinnerung an Git Updater
-register_activation_hook(__FILE__, 'wphgb_activate'); // Funktions-Name anpassen
-function wphgb_activate()
+// Plugin ersetzen
+register_activation_hook(__FILE__, 'test_plugin_activate'); // Funktions-Name anpassen
+function test_plugin_activate()
 { // Funktions-Name anpassen
     $to = get_option('admin_email');
-    $subject = 'Plugin "WP H-Guestbook"'; // Plugin-Name anpassen
-    $message = 'Falls nicht vorhanden:
-Bitte das Plugin "Git Updater" hier https://herbrand.org/tutorials/github/git-updater/ herunterladen,
-installieren und aktivieren, um weiterhin Updates zu erhalten!';
+    $subject = 'Plugin "Test-Plugin"'; // Plugin-Name anpassen
+    $message = 'Bitte beachten:
+Nach dieser Anleitung vorgehen: https://herbrand.org/wordpress/eigene-plugins/wp-h-data-protection/!';
     wp_mail($to, $subject, $message);
 }
